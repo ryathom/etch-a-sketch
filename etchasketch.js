9c7795c02a0ded1ccc46019f24113384df2a7876
Ryan Thomas <ryanthomasj15@gmail.com>
2022-09-05 21:42:30 +0100

    Grid regeneration and variable resolution

--- etchasketch.js
+++ etchasketch.js
@@ -1,15 +1,23 @@
 // Objects
 const grid = document.querySelector('.grid');
 
+const regen = document.querySelector('.regen');
+regen.addEventListener('click', regenerateGrid);
+
+
 // Functions
 function populateGrid(n) {
   for (let i = 0; i < n; i++) {
-    const pixel = document.createElement('div');
-
-    pixel.classList.add('pixel');
-    pixel.addEventListener('mouseover', fillPixel);
-
-    grid.appendChild(pixel);
+    for (let j = 0; j < n; j++) {
+      const pixel = document.createElement('div');
+  
+      pixel.classList.add('pixel');
+      pixel.style.width= (480/n) + 'px';
+      pixel.style.height= (480/n) + 'px';
+      pixel.addEventListener('mouseover', fillPixel);
+  
+      grid.appendChild(pixel);
+    }
   }
 }
 
@@ -18,7 +26,18 @@ function fillPixel() {
   this.classList.add('filled');
 }
 
+function regenerateGrid() {
+  let res = parseInt(prompt("Choose a vertical resolution from 1 to 100:", "16"));
+
+  // Clear grid
+  while(grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  }
+
+  populateGrid(res);
+}
+
 // Main
-let resolution = 16*16
+let resolution = 16
 
 populateGrid(resolution);
